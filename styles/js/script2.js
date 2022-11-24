310cd6189c136fc9269d80735ba6b2dcb0f8aeaa
Meo Meo <rayker09@gmail.com>
2022-11-24 18:47:52 +0700

    add new caterory.html

--- styles/js/script2.js
+++ styles/js/script2.js
@@ -1,7 +1,5 @@
 let arrow = document.querySelectorAll(".arrow");
 
-
-
 for (var i = 0; i < arrow.length; i++){
     arrow[i].addEventListener("click", (e)=>{
         let arrowParent = e.target.parentElement.parentElement;
@@ -15,3 +13,53 @@ let sidebarBtn = document.querySelector(".bx-menu");
 sidebarBtn.addEventListener("click", ()=>{
     sidebar.classList.toggle("close");
 });
+
+/** 
+*Sorts a HTML table.
+*
+* @param {HTMLTableElement} table The table to sort
+* @param {number} column the index of the column to sort
+* @param {boolean} asc Determines if the sorting will be n ascending
+*/
+
+function sortTableByColumn(table, column, asc = true) {
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll("tr"));
+
+    //sort each row
+    const sortedRows = rows.sort((a, b) => {
+		const aColText = a.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+		const bColText = b.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+
+		return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+	});
+
+    //remove all existing TRs from the table 
+    while (tBody.firstChild){
+        tBody.removeChild(tBody.firstChild);
+    }
+
+    //Re-add the newly sorted rows
+    tBody.append(...sortedRows);
+
+    //Remember how the coloumn is currently sorted
+    table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-asc", asc);
+    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-desc", !asc);
+
+
+
+}
+
+document.querySelectorAll(".table-sortable th").forEach(headerCell => {
+    headerCell.addEventListener("click", () => {
+        const tableElement = headerCell.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+        const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+
+        sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+    });
+});
+
+// sortTableByColumn(document.querySelector("table"), 1, true);
