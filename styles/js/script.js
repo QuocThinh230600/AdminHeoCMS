60aa5f7a381aa3a4859476246d706e726f15146f
Mèo Méo Meo Mèo Meo <56384303+QuocThinh230600@users.noreply.github.com>
2022-12-01 01:12:16 +0700

    add new project

--- styles/js/script.js
+++ styles/js/script.js
@@ -1,12 +1,55 @@
+    let arrow     = document.querySelectorAll(".arrow");
+    let show_name = document.querySelectorAll(".show_name");
+    let icon_bx   = document.querySelectorAll(".icon_active");
+    let icon_menu = document.querySelector('.bx-menu'); 
+    let sidebar   = document.querySelector(".sidebar");
+    let dark_mode = document.querySelectorAll(".dark-mode");
 
-let btn = document.querySelector("#btn");
-let sidebar = document.querySelector(".sidebar");
-let searchBtn = document.querySelector("fa-magnifying-glass");
+    for (var i = 0; i < arrow.length; i++){
+        arrow[i].addEventListener("click", (e)=>{
+            let arrowParent = e.target.parentElement.parentElement;
+            arrowParent.classList.toggle("show_menu");
+        });
+    }
 
-btn.onclick = function() {
-    sidebar.classList.toggle("active");
-}
+    for (var i = 0; i < show_name.length; i++){
+        show_name[i].addEventListener("click", (e)=>{
+            let iconParent = e.target.parentElement.parentElement.parentElement;
+            iconParent.classList.toggle("show_menu");
+        });
+    }
 
-searchBtn.onclick = function() {
-    sidebar.classList.toggle("active");
-}
+    for (var i = 0; i < icon_bx.length; i++){
+        icon_bx[i].addEventListener("click", (e)=>{
+            let iconParent = e.target.parentElement.parentElement.parentElement;
+            iconParent.classList.toggle("show_menu");
+        });
+    }
+
+    for (var i = 0; i < arrow.length; i++){
+        arrow[i].addEventListener("click", (e)=>{
+            let arrowParent = e.target.parentElement.parentElement;
+            arrowParent.classList.toggle("active");
+        });
+    }
+
+    for (var i = 0; i < show_name.length; i++){
+        show_name[i].addEventListener("click", (e)=>{
+            let iconParent = e.target.parentElement.parentElement.parentElement;
+            iconParent.classList.toggle("active");
+        });
+    }
+
+    for (var i = 0; i < icon_bx.length; i++){
+        icon_bx[i].addEventListener("click", (e)=>{
+            let iconParent = e.target.parentElement.parentElement.parentElement;
+            iconParent.classList.toggle("active");
+        });
+    }
+
+    icon_menu.addEventListener("click", ()=>{
+        sidebar.classList.toggle("close");
+    });
+
+    
+
