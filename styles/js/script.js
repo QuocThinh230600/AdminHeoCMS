1f1083d5bdf2dd81732fc0a103d506da99876c16
Meo Meo <rayker09@gmail.com>
2022-12-02 00:03:56 +0700

    add

--- styles/js/script.js
+++ styles/js/script.js
@@ -26,26 +26,26 @@
         });
     }
 
-    for (var i = 0; i < arrow.length; i++){
-        arrow[i].addEventListener("click", (e)=>{
-            let arrowParent = e.target.parentElement.parentElement;
-            arrowParent.classList.toggle("active");
-        });
-    }
+    // for (var i = 0; i < arrow.length; i++){
+    //     arrow[i].addEventListener("click", (e)=>{
+    //         let arrowParent = e.target.parentElement.parentElement;
+    //         arrowParent.classList.toggle("active");
+    //     });
+    // }
 
-    for (var i = 0; i < show_name.length; i++){
-        show_name[i].addEventListener("click", (e)=>{
-            let iconParent = e.target.parentElement.parentElement.parentElement;
-            iconParent.classList.toggle("active");
-        });
-    }
+    // for (var i = 0; i < show_name.length; i++){
+    //     show_name[i].addEventListener("click", (e)=>{
+    //         let iconParent = e.target.parentElement.parentElement.parentElement;
+    //         iconParent.classList.toggle("active");
+    //     });
+    // }
 
-    for (var i = 0; i < icon_bx.length; i++){
-        icon_bx[i].addEventListener("click", (e)=>{
-            let iconParent = e.target.parentElement.parentElement.parentElement;
-            iconParent.classList.toggle("active");
-        });
-    }
+    // for (var i = 0; i < icon_bx.length; i++){
+    //     icon_bx[i].addEventListener("click", (e)=>{
+    //         let iconParent = e.target.parentElement.parentElement.parentElement;
+    //         iconParent.classList.toggle("active");
+    //     });
+    // }
 
     icon_menu.addEventListener("click", ()=>{
         sidebar.classList.toggle("close");
