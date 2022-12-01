a6387f20d6f75df5268acb4089c38c290aa463ac
Mèo Méo Meo Mèo Meo <56384303+QuocThinh230600@users.noreply.github.com>
2022-12-02 00:47:52 +0700

    add js

--- styles/js/script.js
+++ styles/js/script.js
@@ -1,52 +1,126 @@
     let arrow     = document.querySelectorAll(".arrow");
     let show_name = document.querySelectorAll(".show_name");
     let icon_bx   = document.querySelectorAll(".icon_active");
+    let dark_mode = document.querySelectorAll(".dark-mode");
     let icon_menu = document.querySelector('.bx-menu'); 
     let sidebar   = document.querySelector(".sidebar");
-    let dark_mode = document.querySelectorAll(".dark-mode");
 
+    const el      = document.getElementById("sidebar-id");
+
+    /**
+     * luuthinh135@gmail.com
+     * @param {element} element 
+     * @param {class} cls 
+     * @returns 
+     */
+    function hasClass(element, cls) {
+        return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+    }
+
+   
+    /**
+     * luuthinh135@gmail.com
+     * show menu sidebar
+     */
     for (var i = 0; i < arrow.length; i++){
         arrow[i].addEventListener("click", (e)=>{
             let arrowParent = e.target.parentElement.parentElement;
-            arrowParent.classList.toggle("show_menu");
+
+            if(hasClass(el, "close")){
+                
+            } else{
+                arrowParent.classList.toggle("show_menu");
+            }
         });
     }
 
+    /**
+     * luuthinh135@gmail.com
+     * show menu sidebar
+     */
     for (var i = 0; i < show_name.length; i++){
         show_name[i].addEventListener("click", (e)=>{
-            let iconParent = e.target.parentElement.parentElement.parentElement;
-            iconParent.classList.toggle("show_menu");
+            let showNameParent = e.target.parentElement.parentElement.parentElement;
+
+            if(hasClass(el, "close")){
+                
+            } else{
+                showNameParent.classList.toggle("show_menu");
+            }
         });
     }
 
+    /**
+     * luuthinh135@gmail.com
+     * show menu sidebar
+     */
     for (var i = 0; i < icon_bx.length; i++){
         icon_bx[i].addEventListener("click", (e)=>{
-            let iconParent = e.target.parentElement.parentElement.parentElement;
-            iconParent.classList.toggle("show_menu");
+            let iconBxParent = e.target.parentElement.parentElement.parentElement;
+
+            if(hasClass(el, "close")){
+                
+            } else{
+                iconBxParent.classList.toggle("show_menu");
+            }
         });
     }
 
-    // for (var i = 0; i < arrow.length; i++){
-    //     arrow[i].addEventListener("click", (e)=>{
-    //         let arrowParent = e.target.parentElement.parentElement;
-    //         arrowParent.classList.toggle("active");
-    //     });
-    // }
+    /**
+     * luuthinh135@gmail.com
+     * active li menu sidebar
+     */
+    for (var i = 0; i < arrow.length; i++){
+        arrow[i].addEventListener("click", (e)=>{
 
-    // for (var i = 0; i < show_name.length; i++){
-    //     show_name[i].addEventListener("click", (e)=>{
-    //         let iconParent = e.target.parentElement.parentElement.parentElement;
-    //         iconParent.classList.toggle("active");
-    //     });
-    // }
+            let arrowParent = e.target.parentElement.parentElement;
 
-    // for (var i = 0; i < icon_bx.length; i++){
-    //     icon_bx[i].addEventListener("click", (e)=>{
-    //         let iconParent = e.target.parentElement.parentElement.parentElement;
-    //         iconParent.classList.toggle("active");
-    //     });
-    // }
+            if(hasClass(el, "close")){
+                
+            } else{
+                arrowParent.classList.toggle("active");
+            }
+        });
+    }
 
+    /**
+     * luuthinh135@gmail.com
+     * active li menu sidebar
+     */
+    for (var i = 0; i < show_name.length; i++){
+        show_name[i].addEventListener("click", (e)=>{
+
+            let showNameParent = e.target.parentElement.parentElement.parentElement;
+
+            if(hasClass(el, "close")){
+                
+            } else{
+                showNameParent.classList.toggle("active");
+            }
+        });
+    }
+
+    /**
+     * luuthinh135@gmail.com
+     * active li menu sidebar
+     */
+    for (var i = 0; i < icon_bx.length; i++){
+        icon_bx[i].addEventListener("click", (e)=>{
+
+            let iconBxParent = e.target.parentElement.parentElement.parentElement;
+
+            if(hasClass(el, "close")){
+                
+            } else {
+                iconBxParent.classList.toggle("active");
+            }
+        });
+    }
+
+    /**
+     * luuthinh135@gmail.com
+     * close menu sidebar
+     */
     icon_menu.addEventListener("click", ()=>{
         sidebar.classList.toggle("close");
     });
